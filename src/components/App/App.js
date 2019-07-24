5349819e679e4ecdc3a840cf57b6ab5a2a086c2d
AjeetY <ajeety@hcl.com>
2019-07-24 09:51:04 +0530

    initial app

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Layout from '../../container/Layout/Layout';
+
+function App() {
+  return (
+    <div >
+      <Layout>
+
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
