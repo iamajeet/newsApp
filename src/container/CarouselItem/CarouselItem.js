5e28f341d3129ed0f20fb7fa58bf3aed60a7079d
AjeetY <ajeety@hcl.com>
2019-07-29 16:39:00 +0530

    backUp

--- src/container/CarouselItem/CarouselItem.js
+++ src/container/CarouselItem/CarouselItem.js
@@ -4,7 +4,7 @@ class CarouselItem extends Component {
     render() {
         return (
             <div class={"carousel-item" + (this.props.active ? ' active' : '')}>
-                <img src={this.props.article.urlToImage} class="d-block w-100" style={{ height: "500px" }} alt="..." />
+                <img src={this.props.article.urlToImage} class="d-block w-100 img-fluid" style={{ height: "500px" }} alt="..." />
                 <div class="carousel-caption  d-md-block">
                     <h5>{this.props.article.title}</h5>
                     <p>{this.props.article.description}</p>
